3e4829063a729b377b2a73c8ebfe477a56caf1f3
Samuel Oliveira <alves.samuca08@gmail.com>
2026-03-17 10:27:12 -0300

    media.cpp

---
+++ media.cpp
@@ -0,0 +1,17 @@
+//Exercício de Média Aritimética
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1;
+    cout << "Insira o Número 1: ";
+    cin >> num1;
+    
+    float num2;
+    cout << "Insira o Número 2: ";
+    cin >> num2;
+    
+    float soma =(num1 +num2)/2;
+    cout <<"Média: ";
+    cout << soma;
+}
